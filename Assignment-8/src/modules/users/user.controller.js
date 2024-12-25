3798b8fe38502a16e93d405861c18384b839ef1b
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2024-12-26 00:33:40 +0200

    finish user apis

--- Assignment-8/src/modules/users/user.controller.js
+++ Assignment-8/src/modules/users/user.controller.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getAllUsers } from "./user.service.js";
+import { createOrUpdateUser, getUserByEmail, getUserById, signUp } from "./user.service.js";
 const userRouter = Router();
 
-
-userRouter.get("/", getAllUsers)
-
-
+userRouter.post("/sign-up", signUp);
+userRouter.put("/create-or-update-user/:id", createOrUpdateUser);
+userRouter.get("/get-user-by-email", getUserByEmail);
+userRouter.get("/get-user-by-id/:id", getUserById);
 
 export default userRouter;
--- Assignment-8/src/modules/users/user.service.js
+++ Assignment-8/src/modules/users/user.service.js
@@ -1,14 +1,71 @@
 import userModel from "../../DB/models/user.model.js";
 import { errorHandling } from "../../utils/errorHandling.js";
 
-
-export const getAllUsers = async (req, res, next) => {
+export const signUp = async (req, res) => {
     try {
-        // const users=await userModel.findAll()
-        const users = await userModel.create(req.body)
-        return res.json({ msg: "ok", users });
+        const { name, email, role, password } = req.body;
+        if (await userModel.findOne({ where: { email } })) {
+            return res.status(400).json({ msg: "Email already exists." });
+        }
+        const newUser = userModel.build({ name, email, role, password, });
+        await newUser.save();
+        return res.status(201).json({ msg: "User created successfully.", user: newUser });
     } catch (error) {
-        errorHandling(error, res)
-
+        errorHandling(error, res);
     }
-}
+};
+
+
+export const createOrUpdateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, email, role, password } = req.body;
+
+        let user = await userModel.findByPk(id);
+        if (user) {
+            await user.update({ name, email, role, password }, { skipValidation: true });
+        } else {
+            user = userModel.build({ id, name, email, role, password });
+            await user.save({ validate: false });
+        }
+        return res.status(200).json({ message: "User created or updated successfully.", user: user });
+    } catch (error) {
+        errorHandling(error, res);
+    }
+};
+
+export const getUserByEmail = async (req, res) => {
+    try {
+        const { email } = req.query;
+        if (!email) {
+            return res.status(400).json({ msg: "Email query parameter is required." });
+        }
+
+        const user = await userModel.findOne({ where: { email } });
+        if (!user) {
+            return res.status(404).json({ msg: "User not found." });
+        }
+
+        return res.status(200).json({ msg: "User found.", user });
+    } catch (error) {
+        errorHandling(error, res);
+    }
+};
+
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const user = await userModel.findByPk(id, {
+            attributes: { exclude: ['role'] }
+        });
+
+        if (!user) {
+            return res.status(404).json({ msg: "User not found." });
+        }
+
+        return res.status(200).json({ msg: "User found.", user });
+    } catch (error) {
+        errorHandling(error, res);
+    }
+};
