585a463769eb0a0659dfe389799acea105e8c14c
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2024-12-30 00:23:49 +0200

    add association with tables

---
+++ Assignment-8/src/DB/associations.js
@@ -0,0 +1,16 @@
+import  userModel  from "./models/user.model.js";
+import  Post from "./models/post.model.js";
+import  Comment  from "./models/comment.model.js";
+
+const setupAssociations = () => {
+    userModel.hasMany(Post, { foreignKey: "userId" });
+    Post.belongsTo(userModel, { foreignKey: "userId" });
+
+    Post.hasMany(Comment, { foreignKey: "postId" });
+    Comment.belongsTo(Post, { foreignKey: "postId" });
+
+    userModel.hasMany(Comment, { foreignKey: "userId" });
+    Comment.belongsTo(userModel, { foreignKey: "userId" });
+};
+
+export default setupAssociations;
--- Assignment-8/src/app.controller.js
+++ Assignment-8/src/app.controller.js
@@ -2,6 +2,7 @@ import { checkConnectionDB, checkSyncDB } from "./DB/connectionDB.js";
 import commentRouter from "./modules/comments/comment.controller.js";
 import postRouter from "./modules/posts/post.controller.js";
 import userRouter from "./modules/users/user.controller.js";
+import setupAssociations from "./DB/associations.js";
 
 const bootstrap = (app, express) => {
     app.use(express.json());
@@ -10,8 +11,10 @@ const bootstrap = (app, express) => {
     app.use("/posts", postRouter);
     app.use("/comments", commentRouter);
 
-    checkConnectionDB()
-    checkSyncDB()
+    checkConnectionDB();
+    checkSyncDB();
+    setupAssociations();
+
     app.use("*", (req, res, next) => {
         res.status(404).json({ message: `${req.originalUrl} not found` });
     })
