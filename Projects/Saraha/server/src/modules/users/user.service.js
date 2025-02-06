379a21c835700e891e921313f9480dfd7e8039e1
Samy Mostafa Shaawat <samy.shaawat@gmail.com>
2025-02-06 06:39:13 +0200

    add crypto package

--- Projects/Saraha/server/src/modules/users/user.service.js
+++ Projects/Saraha/server/src/modules/users/user.service.js
@@ -1,5 +1,5 @@
 import userModel from "../../DB/models/user.model.js"
-
+import bcrypt from "bcrypt"
 export const signUp = async (req, res, next) => {
     try {
         const { name, email, password, cPassword, phone, gender } = req.body
@@ -14,10 +14,14 @@ export const signUp = async (req, res, next) => {
         if (emailExist) {
             return res.status(409).json({ msg: "Email already exists." })
         }
+        // hash password
+        const hashPassword = bcrypt.hashSync(password, 12)
+        console.log(hashPassword);
 
+        // 
         // create a new user
-        const user = await userModel.create({ name, email, password, phone, gender })
-        
+        const user = await userModel.create({ name, email, password: hashPassword, phone, gender })
+
         return res.status(201).json({ msg: "done", user })
     } catch (error) {
         return res.status(500).json({ msg: "Error: ", message: error.message, stack: error.stack, error });
